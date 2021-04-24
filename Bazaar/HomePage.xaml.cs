c460f0fd931c736ca21700cba7f763f2bef794ff
Akhila Johnson C <akhilajohnsonc@gmail.com>
2021-04-24 20:09:45 +0530

    Add project files.

---
+++ Bazaar/HomePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Bazaar.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bazaar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        ObservableCollection<CarouselClass> Datas;
+
+        public HomePage()
+        {
+            Datas = new ObservableCollection<CarouselClass>();
+            InitializeComponent();
+            Datas.Add(new CarouselClass
+            {
+                viewtext = "Hello",
+                viewimage = "Rectangle.png"
+            });
+
+            Datas.Add(new CarouselClass
+            {
+                viewtext = "Hello",
+                viewimage = "Rectangle.png"
+            });
+
+            Datas.Add(new CarouselClass
+            {
+                viewtext = "Hello",
+                viewimage = "Rectangle.png"
+            });
+            MainCarouselView.ItemsSource = Datas;
+        }
+    }
+}
---
+++ Bazaar/Model/CarouselClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bazaar.Model
+{
+    public class CarouselClass
+    {
+        public string viewimage { get; set; }
+        public string viewtext { get; set; }
+    }
+}
